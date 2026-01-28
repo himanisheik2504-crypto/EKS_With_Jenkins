2f08f643572b764eca67dd07a953429333ce8cea
himanisheik2504-crypto <himanisheik2504@gmail.com>
2026-01-28 15:10:24 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,6 +3,6 @@ module "eks_cluster" {
   version         = "20.0.0"
   cluster_name    = "my-cluster-poc-13"
   cluster_version = "1.28"
-  subnets         = module.vpc_main.public_subnets
   vpc_id          = module.vpc_main.vpc_id
+  vpc_subnets         = module.vpc_main.public_subnets
 }
