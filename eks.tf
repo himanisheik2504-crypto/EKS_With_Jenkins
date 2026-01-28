0b83e9a0dad6e5b83093c2e5b131fdeaf15153d9
himanisheik2504-crypto <himanisheik2504@gmail.com>
2026-01-28 14:59:35 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 module "eks_cluster" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.0.0"
-  cluster_name    = "my-cluster"
+  cluster_name    = "my-cluster-poc-13"
   cluster_version = "1.28"
   subnets         = module.vpc_main.public_subnets
   vpc_id          = module.vpc_main.vpc_id
