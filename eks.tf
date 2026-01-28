f1631442544fdd2a4e6b248a47e62cec4a28d9d6
himanisheik2504-crypto <himanisheik2504@gmail.com>
2026-01-28 14:57:23 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,15 +1,8 @@
 module "eks_cluster" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.5.1"
- 
-  name = "simple-eks-vpc"
-  cidr = "10.0.0.0/16"
- 
-  azs             = ["us-east-2a", "us-east-2b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
- 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.0.0"
+  cluster_name    = "my-cluster"
+  cluster_version = "1.28"
+  subnets         = module.vpc_main.public_subnets
+  vpc_id          = module.vpc_main.vpc_id
 }
- 
